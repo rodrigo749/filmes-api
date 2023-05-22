4eb4ece85f1dcd8ebdd645489238d153d38cac0d
Rodrigo Neves <{ID}+{username}@users.noreply.github.com>
2023-05-22 19:21:32 -0300

    create first migration

---
+++ FilmesApi/Migrations/20230522221031_CriandoTabelaDeFilmes.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesApi.Migrations
+{
+    public partial class CriandoTabelaDeFilmes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Faturamento",
+                table: "Filmes");
+
+            migrationBuilder.DropColumn(
+                name: "Lancamento",
+                table: "Filmes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Filmes",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genero",
+                table: "Filmes",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Diretor",
+                table: "Filmes",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Filmes",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genero",
+                table: "Filmes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Diretor",
+                table: "Filmes",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Faturamento",
+                table: "Filmes",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Lancamento",
+                table: "Filmes",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
